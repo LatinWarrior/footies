2e37440060a81a151146daa12ab0d965c01683de
Luis Blanco <luisblanco@Luiss-MacBook-Pro-2.local>
2024-02-04 12:31:53 -0600

    Fixed bugs. Added code for slug pages.

--- app/meals/[MealSlug]/page.jsx
+++ app/meals/[MealSlug]/page.jsx
@@ -1,14 +1,46 @@
-import React from 'react';
-import Link from 'next/link';
+import Image from 'next/image';
+import { notFound } from 'next/navigation'
 
-const MealsPage = () => {
+import { getMeal } from '@/lib/meals';
+
+import classes from './page.module.css';
+
+const MealDetailsPage = ({ params }) => {
+    console.log(`params: `, params);
+    const meal = getMeal(params.mealSlug);
+
+    if (!meal) {
+        notFound();
+    }
+
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
     return (
-        <main>
-            <p className='Link'>
-                <Link href='/meal-1'>Meal 1</Link>
-            </p>
-        </main>
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} fill alt={meal.title} />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{4}</h1>
+                    <p className={classes.creator}>
+                        by{' '}
+                        <a href={`mailto:${meal.creator_email}`}>
+                            {meal.creator}
+                        </a>
+                    </p>
+                    <p className={classes.summary}>{meal.summary}</p>
+                </div>
+            </header>
+            <main>
+                <p
+                    className={classes.instructions}
+                    dangerouslySetInnerHTML={{
+                        __html: meal.instructions,
+                    }}></p>
+            </main>
+        </>
     );
 };
 
-export default MealsPage;
+export default MealDetailsPage;
